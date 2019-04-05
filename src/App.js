e24d73b070edcc2075ad811764d0753d32d9932b
Aleks Mishev <33548921+alexalex972@users.noreply.github.com>
2019-04-05 17:56:18 +0300

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+    this.inputRef1 = React.createRef();
+    this.state = { f1: 0, f2: 0, r:0 };
+    this.sum = () => {
+      this.setState({
+        r: this.state.f1 + this.state.f2
+      });
+    };
+    this.onChange = (name) => (event) =>{
+      this.setState({
+        [name]: +(event.target.value)
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <input value={this.state.f1} onChange={this.onChange('f1')} />
+        +
+        <input value={this.state.f2} onChange={this.onChange('f2')} />
+        <button onClick={this.sum}> = </button>
+        <span>Value: {this.state.r}</span>
       </div>
     );
   }
 }
 
 export default App;
+
+
